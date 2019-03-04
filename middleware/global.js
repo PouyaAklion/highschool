7696960a4555308c86693718d7e8904839fd5635
PouyaAklion <pouyaakn4@gmail.com>
2019-03-04 22:43:42 +0330

    update global middleware for auth and locale language

--- middleware/global.js
+++ middleware/global.js
@@ -1,13 +1,19 @@
 const { User } = require('../models/user');
 let global = function (req, res, next) {
+	req.locale = 'fa';
 	let token = req.cookies.access_token;
-	User.findByToken(token).then((user) => {
-		req.user = user;
-		req.token = token;
+	if (token) {
+		User.findByToken(token).then((user) => {
+			req.user = user;
+			req.token = token;
+			next();
+		}).catch(e => {
+			next();
+		})
+	} else {
 		next();
-	}).catch(e => {
-		next();
-	})
+	}
+
 }
 module.exports = {
 	global
