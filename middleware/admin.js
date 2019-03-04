e948c50ffba78ef71f4bfcebe7b29cd3dc403afe
PouyaAklion <pouyaakn4@gmail.com>
2019-03-04 22:43:03 +0330

    just use req.user.role for specify admin

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,17 +1,10 @@
 const { User } = require('../models/user');
 let admin = function (req, res, next) {
-	let token = req.cookies.access_token;
-	User.findByToken(token).then((user) => {
-		
-		if(user.role !== 'admin'){
-			throw new Error();
-		}
-		req.user = user;
-		req.token = token;
+	if (req.user.role === 'admin') {
 		next();
-	}).catch(e => {
+	} else {
 		res.redirect('/');
-	})
+	}
 }
 module.exports = {
 	admin
